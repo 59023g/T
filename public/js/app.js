947561b1f8808900135fef730de45c466b253004
mp1erce <hell@michaelepierce.com>
2014-03-09 22:30:25 -0700

    App

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,49 @@
 'use strict';
 
-angular.module('tint', [
+var tint = angular.module('tint', [
   //'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ])
-  .config(function ($routeProvider, $httpProvider) {
+  .config(function($routeProvider, $httpProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
-        controller: 'doController'
+        controller: 'doController',
       })
       .when('/learn', {
         templateUrl: 'partials/learn',
-        controller: 'doController'
+        controller: 'doController',
+        css: 'css/learn.css'
       })
+      .when('/pricing', {
+        templateUrl: 'partials/pricing',
+        controller: 'doController',
+        //animate: 'slideLeft'
+      })
+      .when('/one', {
+        templateUrl: 'partials/one',
+        controller: 'doController',
+        css: 'css/slide.css'
+      })
+      .when('/two', {
+        templateUrl: 'partials/two',
+        controller: 'doController',
+        //animate: 'slideLeft',
+        css: 'css/slide2.css'
+      }) 
+      .when('/three', {
+        templateUrl: 'partials/three',
+        controller: 'doController',
+        //animate: 'slideLeft',
+        css: 'css/slide3.css'
+      })            
       .otherwise({
         redirectTo: '/'
       });
 
+    $locationProvider.html5Mode(true)
 
   });
-
-
--- public/js/controllers/doController.js
+++ public/js/controllers/doController.js
@@ -1,7 +1,27 @@
  'use strict';
 
  angular.module('tint')
-   .controller('doController', function($scope) {
+   .controller('doController', function($scope, $location) {
+   	   $RootScope.go = function (path, pageAnimationClass) {
+
+        if (typeof(pageAnimationClass) === 'undefined') { // Use a default, your choice
+            $RootScope.pageAnimationClass = 'crossFade';
+        }
+        
+        else { // Use the specified animation
+            $RootScope.pageAnimationClass = pageAnimationClass;
+        }
+
+        if (path === 'back') { // Allow a 'back' keyword to go to previous page
+            $window.history.back();
+        }
+        
+        else { // Go to the specified path
+            $location.path(path);
+        }
+    };
+        
+
 
    });
 
